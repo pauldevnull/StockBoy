a4bce0da5d6d9686276a363210fcfc39fbf4619d
Paul Hutchinson <phutchinson@labelinsight.com>
2016-11-12 14:48:18 -0600

    updated gitignore to ignore .txt output files and cleaned up logging

--- src/main/java/com/paulmhutchinson/StockBoyApplication.java
+++ src/main/java/com/paulmhutchinson/StockBoyApplication.java
@@ -1,16 +1,28 @@
 package com.paulmhutchinson;
 
 import com.paulmhutchinson.domain.result.Result;
-import com.paulmhutchinson.service.result.ResultService;
+import com.paulmhutchinson.domain.status.Status;
 import com.paulmhutchinson.service.filewriter.FileWriterService;
+import com.paulmhutchinson.service.result.ResultService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import yahoofinance.YahooFinance;
+
+import java.util.logging.Level;
 
 public class StockBoyApplication {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StockBoyApplication.class);
+
     public static void main(String[] args) throws Exception {
+        YahooFinance.logger.setLevel(Level.OFF);
+
+        LOGGER.info(Status.STARTING.getMessage());
         ResultService resultService = new ResultService();
         Result result = resultService.getResult();
 
         FileWriterService fileWriterService = new FileWriterService(result);
         fileWriterService.write();
+        LOGGER.info(Status.COMPLETED.getMessage());
     }
 }
--- src/main/java/com/paulmhutchinson/domain/status/Status.java
+++ src/main/java/com/paulmhutchinson/domain/status/Status.java
@@ -2,18 +2,20 @@ package com.paulmhutchinson.domain.status;
 
 public enum Status {
 
-    READING_SYMBOLS("\t\t\tReading symbol list..."),
-    RETRIEVING_STOCKS("\t\tRetrieving stocks..."),
-    RETRIEVING_RESULT("\tRetrieving result..."),
-    FILTERING_STOCKS("\t\tFiltering stocks..."),
-    APPLYING_FILTER("\t\t\t\t\tApplying {} filter with value(s) {}..."),
-    WRITING("\tWriting to file..."),
-    ERROR_READING_SYMBOLS("\tError reading symbol list"),
-    ERROR_RETRIEVING_STOCKS("\tError retrieving stocks"),
-    ERROR_RETRIEVING_RESULT("\tError retrieving result"),
-    ERROR_FILTERING_STOCKS("\tError filtering stocks"),
-    ERROR_APPLYING_FILTER("\t\t\tError applying {} filter with value(s) {}"),
-    ERROR_WRITING("\tError writing to file");
+    STARTING("\t\t\tStarting..."),
+    READING_SYMBOLS("\t\t\t\tReading symbol list..."),
+    RETRIEVING_STOCKS("\t\t\tRetrieving stocks..."),
+    RETRIEVING_RESULT("\t\tRetrieving result..."),
+    FILTERING_STOCKS("\t\t\tFiltering stocks..."),
+    APPLYING_FILTER("\t\t\t\t\t\tApplying {} filter with value(s) {}..."),
+    WRITING("\t\tWriting to file..."),
+    ERROR_READING_SYMBOLS("\t\tError reading symbol list"),
+    ERROR_RETRIEVING_STOCKS("\t\tError retrieving stocks"),
+    ERROR_RETRIEVING_RESULT("\t\tError retrieving result"),
+    ERROR_FILTERING_STOCKS("\t\tError filtering stocks"),
+    ERROR_APPLYING_FILTER("\t\t\t\tError applying {} filter with value(s) {}"),
+    ERROR_WRITING("\t\tError writing to file"),
+    COMPLETED("\t\t\tCompleted");
 
     private String message;
 
